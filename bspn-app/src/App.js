b8cb0f358d16fba1166237ab8668330543832f1b
jacklayfield <layf100@gmail.com>
2022-03-04 17:21:12 -0500

    Started react work

--- bspn-app/src/App.js
+++ bspn-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PageLayout from "../src/PageLayout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>BSPN</h2>
+        <h3>Radford City Sports</h3>
+        <PageLayout></PageLayout>
+      </div>
+    );
+  }
 }
 
 export default App;
