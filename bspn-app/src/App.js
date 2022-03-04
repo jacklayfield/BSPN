f21605538b436fbaea4977a73d52b51c72053de0
jacklayfield <layf100@gmail.com>
2022-03-04 18:37:18 -0500

    Added a basic nav bar and page

--- bspn-app/src/App.js
+++ bspn-app/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PageLayout from "../src/PageLayout";
+import PageLayout from "./components/pageLayout";
+import Hockey from "./pages/hockey";
+import NavBar from "./components/navBar";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <NavBar />
         <h2>BSPN</h2>
         <h3>Radford City Sports</h3>
         <PageLayout></PageLayout>
---
+++ bspn-app/src/pages/hockey.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Hockey = () => {
+  return (
+    <div>
+      <h3>Hockey</h3>
+      <div>
+        <img src="./hockey.jpg" />
+      </div>
+    </div>
+  );
+};
+export default Hockey;
