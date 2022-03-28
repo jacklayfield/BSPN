746bb1c02781d564c34eb47f67ab3a838b9d064a
jacklayfield <layf100@gmail.com>
2022-03-27 23:01:31 -0400

    restructured backend (now api), added login/register, added models,routes

---
+++ api/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bspnRoutes = express.Router();
+const PORT = 4000;
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+app.use(express.json());
+
+let Score = require("./bspn.model");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// mongoose.connect("mongodb://127.0.0.1:27017/bspn", { useNewUrlParser: true });
+// const connection = mongoose.connection;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+
+bspnRoutes.route("/").get(function (req, res) {
+  Score.find(function (err, scores) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(scores);
+    }
+  });
+});
+
+bspnRoutes.route("/:id").get(function (req, res) {
+  let id = req.params.id;
+  Score.findById(id, function (err, score) {
+    res.json(score);
+  });
+});
+
+bspnRoutes.route("/add").post(function (req, res) {
+  let score = new Score(req.body);
+  score
+    .save()
+    .then((score) => {
+      res.status(200).json({ score: "score successfully added" });
+    })
+    .catch((err) => {
+      res.status(400).send("adding score failed");
+    });
+});
+
+// app.use("/bspn", bspnRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server successfully started, running on Port: " + PORT);
+});
---
+++ api/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
