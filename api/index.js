8594cae6ec1a89d6d2aca65e120bb55e6f8b1680
jacklayfield <layf100@gmail.com>
2022-03-29 01:30:33 -0400

    Basic API completed

--- api/index.js
+++ api/index.js
@@ -2,22 +2,24 @@
 
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 const mongoose = require("mongoose");
-const bspnRoutes = express.Router();
+// const bspnRoutes = express.Router();
 const PORT = 4000;
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
 
-let Score = require("./bspn.model");
+// let Score = require("./bspn.model");
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 // mongoose.connect("mongodb://127.0.0.1:27017/bspn", { useNewUrlParser: true });
 // const connection = mongoose.connection;
@@ -29,37 +31,52 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, "144136.jpg");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
 
-bspnRoutes.route("/").get(function (req, res) {
-  Score.find(function (err, scores) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(scores);
-    }
-  });
-});
+// bspnRoutes.route("/").get(function (req, res) {
+//   Score.find(function (err, scores) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json(scores);
+//     }
+//   });
+// });
 
-bspnRoutes.route("/:id").get(function (req, res) {
-  let id = req.params.id;
-  Score.findById(id, function (err, score) {
-    res.json(score);
-  });
-});
+// bspnRoutes.route("/:id").get(function (req, res) {
+//   let id = req.params.id;
+//   Score.findById(id, function (err, score) {
+//     res.json(score);
+//   });
+// });
 
-bspnRoutes.route("/add").post(function (req, res) {
-  let score = new Score(req.body);
-  score
-    .save()
-    .then((score) => {
-      res.status(200).json({ score: "score successfully added" });
-    })
-    .catch((err) => {
-      res.status(400).send("adding score failed");
-    });
-});
+// bspnRoutes.route("/add").post(function (req, res) {
+//   let score = new Score(req.body);
+//   score
+//     .save()
+//     .then((score) => {
+//       res.status(200).json({ score: "score successfully added" });
+//     })
+//     .catch((err) => {
+//       res.status(400).send("adding score failed");
+//     });
+// });
 
 // app.use("/bspn", bspnRoutes);
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const PostModel = require("../models/Post");
+const Post = require("../models/Post");
 
 //UPDATE
 router.put("/:id", async (req, res) => {
@@ -10,9 +12,13 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
@@ -23,4 +29,34 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ author: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found");
+    }
+  } else {
+    res.status(401).json("Unauthorized attempt to delete an account");
+  }
+});
+
+//GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
