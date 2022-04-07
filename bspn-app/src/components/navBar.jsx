b2e7d111bca514828746ca50a6b4cd57667b85e0
jnowacki24 <73665603+jnowacki24@users.noreply.github.com>
2022-04-07 16:08:16 -0400

    navbar style update

--- bspn-app/src/components/navBar.jsx
+++ bspn-app/src/components/navBar.jsx
@@ -45,7 +45,10 @@ export function NavBar() {
             </a>
           </li>
           <li className="nav-item active">
-            <a className="nav-link" href="/schedule">
+            <a style={{
+                fontWeight: "bold",
+              }}
+              className="nav-link" href="/schedule">
               Schedule
             </a>
           </li>
