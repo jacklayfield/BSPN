38a242b1b7d84bf7177698dafefe9fbe6d888155
jnowacki24 <73665603+jnowacki24@users.noreply.github.com>
2022-04-06 20:34:24 -0400

    roster files

---
+++ api/models/Roster.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const RosterSchema = new mongoose.Schema(
+  {
+    sport: {
+        type: String,
+        required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: false,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Roster", RosterSchema);
