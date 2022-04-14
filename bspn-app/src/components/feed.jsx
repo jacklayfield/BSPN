c35f45955767ab33464374fd39a4f8b6cdfd4400
Wyatt Fails <wyattfails@MacBook-Pro-5.local>
2022-04-14 13:18:01 -0400

    cleaning up code

--- bspn-app/src/components/feed.jsx
+++ bspn-app/src/components/feed.jsx
@@ -13,27 +13,6 @@ import Articles from "../components/articles";
 
 export function Feed({ articles }) {
   return (
-    /*<div id="flex" className="row">
-      <div className="second-third">
-        <div id="score-bar">
-          <h2>Recent Scores</h2>
-          <div class="grid-container">
-            <Score />
-            <Score />
-          </div>
-        </div>
-      </div>
-      <div className="first-third">
-        <Articles articles={articles} />
-        {}
-      </div>
-      <div className="third-third video">
-        <h2>Latest Broadcasts</h2>
-        <div class="grid-container">
-          <Broadcast />
-        </div>
-      </div>
-    </div>*/
     <div>
       <Row className="gx-0">
         <Col
